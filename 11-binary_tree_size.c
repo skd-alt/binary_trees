74deecb1bd47e65adfab462ee168a271216f7d6a
skd-alt <sipho@skanalytics.co.za>
2023-11-02 14:01:08 -0700

    Size of tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - calculates size of tree
+ * @tree: point to tree
+ *
+ * Return: size of tree
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0, size_r = 0, size_l = 0;
+
+	if (tree == NULL)
+		return (0);
+	size_l = binary_tree_size(tree->left);
+	size_r = binary_tree_size(tree->right);
+	size = size_l + size_r + 1;
+	return (size);
+}
